c049486006317b422a5b04adcf6fc6fa98541a18
Fredrik Gjellestad <fredrik@nerdiverset.no>
2026-09-17 14:44:33 +0200

    Added directorybuster and other minor fixes

--- directorybuster.py
+++ directorybuster.py
@@ -1,25 +1,42 @@
+import requests
+from tqdm import tqdm
+
+
 class directoryBuster:
     def __init__(self):
-        self.target = None
-        self.loot = None
+        self.target = "http://10.1.1.39"
+        self.agent = (
+            "Mozilla/5.0 (X11; Linux x86_64; rv:154.0) Gecko/20100101 Firefox/154.0"
+        )
+        self.loot = []
         self.words = []
 
-    def get_words(self):
-        wordlist = "wordlist/all.txt"
-        extensions = [".php", ".bak", ".orig", ".inc"]
+    def _get_words(self, wordlist: str):
+        words = []
         with open(wordlist) as f:
-            words = f.read().split()
-        for word in words:
+            raw_words = f.read().split()
+        for word in raw_words:
             if "." in word:
-                self.words.append(f"/{word}")
+                words.append(f"/{word}")
             else:
-                self.words.append(f"/{word}/")
+                words.append(f"/{word}/")
+        return words
+
+    def directory_brute(self, target: str, wordlist: str):
+        words = self._get_words(wordlist)
+        headers = {"User-Agent": self.agent}
+        for i in tqdm(words, desc="Brute forcing directories", total=len(words)):
+            url = f"{self.target}{i}"
+            r = requests.get(url, headers=headers)
+            if r.status_code == 200:
+                self.loot.append(url)
 
 
 if __name__ == "__main__":
     t = directoryBuster()
-    t.get_words()
-    print(t.words[:10])
+    t.directory_brute(target="10.1.1.39", wordlist="wordlist/common.txt")
+    for loot in t.loot:
+        print(loot)
 
 
 ### Readlines kept the \n and the end, and split used it to split the words
