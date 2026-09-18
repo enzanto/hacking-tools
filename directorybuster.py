907b38528b8335af9bdbc5ab5bad5959a1992a91
Fredrik Gjellestad <fredrik@nerdiverset.no>
2026-09-18 12:11:39 +0200

    Added docstrings

--- directorybuster.py
+++ directorybuster.py
@@ -12,6 +12,15 @@ class DirectoryBuster:
         self.words = []
 
     def _get_words(self, wordlist: str):
+        """An internal function to provide a list of words
+
+        This functions takes a wordlist provided with one word per line, and splits
+        it in to separate words. Then enclose it in slashes (/) before appending to list.
+
+        args:
+            wordlist (str): A string with the location of the wordlist to open.
+
+        returns (list): A list with words for directory brute"""
         words = []
         with open(wordlist) as f:
             raw_words = f.read().split()
@@ -23,6 +32,19 @@ class DirectoryBuster:
         return words
 
     def directory_brute(self, target: str, wordlist: str):
+        """A function to brute force directory locations from a wordlist
+
+        This function takes the wordlist, and sends in to _get_words to retrieve a list
+        and uses that list with the requests module to check for status_code 200 and
+        store the found directories to loot variable
+
+        args:
+            target (str): The base URL of the targets
+            wordlist (str): The location of the wordlist file
+
+        returns:
+            loot (list[str]): Returns a list of strings with successful directories or files"""
+
         words = self._get_words(wordlist)
         headers = {"User-Agent": self.agent}
         for i in tqdm(words, desc="Brute forcing directories", total=len(words)):
